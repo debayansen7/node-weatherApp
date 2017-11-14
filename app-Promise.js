69c7cab3ead21756c917762b0e3adc13955b9ac2
DebayanSen7 <debayan.sen7@gmail.com>
2017-11-14 09:25:47 +0530

    Updated Axios for fetching api call

--- app-Promise.js
+++ app-Promise.js
@@ -16,11 +16,25 @@ const argv = yargs.options({
 var encodeAddress = encodeURIComponent(argv.address);
 var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`;
 
-// axios.get(geocodeURL).then((response ) => {
-//   console.log(response);
-// })
-
-
-
+axios.get(geocodeURL).then((response) => {
+  if(response.data.status === 'ZERO_RESULTS'){
+    throw new Error("Unable to find that address..");
+  }
+  console.log(response.data.results[0].formatted_address);
+  var lat = response.data.results[0].geometry.location.lat;
+  var lng = response.data.results[0].geometry.location.lng;
+  var weatherURL = `https://api.darksky.net/forecast/98ae296519a16a250ea4bb4546ab43ac/${lat},${lng}`
+  return axios.get(weatherURL);
+}).then((response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It is ${temperature}, but feels like ${apparentTemperature}`);
+}).catch((e) => {
+  if(e.code === 'ENOTFOUND'){
+    console.log("Unable to connect to api server!!!");
+  }else{
+    console.log(e.message);
+  }
+})
 
 //    API key for weather forcast from forecasr.io - 98ae296519a16a250ea4bb4546ab43ac

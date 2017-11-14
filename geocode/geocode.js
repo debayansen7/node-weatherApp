4742af7abfd0dd4115c338a8d49ac452fedbc208
DebayanSen7 <debayan.sen7@gmail.com>
2017-11-14 09:08:26 +0530

    Using Promises

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+// Using Promises
 var geocodeaddress = (address) => {
   return new Promise((resolve, reject) => {
     var encodeAddress = encodeURIComponent(address);
